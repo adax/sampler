04dfce19ed4cc5164aae8ae75d60b7b948e3b971
Gleb Gorelov <gorelov@haulmont.com>
2015-03-16 18:35:19 +0400

    Button samples

--- modules/gui/src/com/haulmont/sampler/gui/components/button/LinkButton.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/button/LinkButton.java
@@ -3,4 +3,8 @@ package com.haulmont.sampler.gui.components.button;
 import com.haulmont.cuba.gui.components.AbstractWindow;
 
 public class LinkButton extends AbstractWindow {
+
+    public void sayHello() {
+        showNotification("Hello, CUBA!", NotificationType.HUMANIZED);
+    }
 }
--- modules/gui/src/com/haulmont/sampler/gui/components/button/SimpleButton.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/button/SimpleButton.java
@@ -3,4 +3,8 @@ package com.haulmont.sampler.gui.components.button;
 import com.haulmont.cuba.gui.components.AbstractFrame;
 
 public class SimpleButton extends AbstractFrame {
+
+    public void sayHello() {
+        showNotification("Hello, CUBA!", NotificationType.HUMANIZED);
+    }
 }
