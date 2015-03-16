a06de70e1e085c813a775928f8fce8d6a96d299d
Gleb Gorelov <gorelov@haulmont.com>
2015-03-16 18:20:31 +0400

    CheckBox samples

---
+++ modules/gui/src/com/haulmont/sampler/gui/components/checkbox/SimpleCheckbox.java
@@ -0,0 +1,27 @@
+package com.haulmont.sampler.gui.components.checkbox;
+
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.CheckBox;
+import com.haulmont.cuba.gui.data.ValueListener;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+public class SimpleCheckbox extends AbstractFrame {
+    @Inject
+    private CheckBox checkBox;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        checkBox.addListener(new ValueListener() {
+            @Override
+            public void valueChanged(Object source, String property, Object prevValue, Object value) {
+                if (Boolean.TRUE.equals(value)) {
+                    showNotification("set", NotificationType.HUMANIZED);
+                } else {
+                    showNotification("not set", NotificationType.HUMANIZED);
+                }
+            }
+        });
+    }
+}
