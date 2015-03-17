10d9b71dfcc4af6c088ecb359c56f533bd4cf394
Gleb Gorelov <gorelov@haulmont.com>
2015-03-17 17:11:43 +0400

    Order entity

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -17,3 +17,21 @@ create table SAMPLER_CUSTOMER (
     primary key (ID)
 )^
 -- end SAMPLER_CUSTOMER
+-- begin SAMPLER_ORDER
+create table SAMPLER_ORDER (
+    ID varchar(36) not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    VERSION integer,
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    CUSTOMER_ID varchar(36),
+    DATE_ date not null,
+    AMOUNT decimal(19, 2),
+    --
+    primary key (ID)
+)^
+-- end SAMPLER_ORDER
