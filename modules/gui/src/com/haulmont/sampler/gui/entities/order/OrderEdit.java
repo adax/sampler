08ce1b91a216c138a6dcba08beeb00245f643a85
Gleb Gorelov <gorelov@haulmont.com>
2015-03-18 10:45:58 +0400

    Order screens

---
+++ modules/gui/src/com/haulmont/sampler/gui/entities/order/OrderEdit.java
@@ -0,0 +1,7 @@
+package com.haulmont.sampler.gui.entities.order;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.sampler.entity.Order;
+
+public class OrderEdit extends AbstractEditor<Order> {
+}
