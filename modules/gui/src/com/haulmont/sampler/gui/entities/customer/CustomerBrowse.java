fba8b5f6b12575a1de55dbb6840e57dfcb4ff23f
Gleb Gorelov <gorelov@haulmont.com>
2015-03-18 10:45:34 +0400

    Customer screens

---
+++ modules/gui/src/com/haulmont/sampler/gui/entities/customer/CustomerBrowse.java
@@ -0,0 +1,6 @@
+package com.haulmont.sampler.gui.entities.customer;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+
+public class CustomerBrowse extends AbstractLookup {
+}
---
+++ modules/gui/src/com/haulmont/sampler/gui/entities/customer/CustomerEdit.java
@@ -0,0 +1,7 @@
+package com.haulmont.sampler.gui.entities.customer;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.sampler.entity.Customer;
+
+public class CustomerEdit extends AbstractEditor<Customer> {
+}
