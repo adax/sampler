93d5398a32570228af42494b80f57141c5f7f97e
Konstantin Krivopustov <krivopustov@haulmont.com>
2015-03-18 17:18:20 +0400

    Example of invoking one method from different buttons.

--- modules/gui/src/com/haulmont/sampler/gui/components/button/LinkButton.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/button/LinkButton.java
@@ -1,10 +1,15 @@
 package com.haulmont.sampler.gui.components.button;
 
 import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.components.Component;
 
 public class LinkButton extends AbstractWindow {
 
     public void sayHello() {
         showNotification("Hello, CUBA!", NotificationType.HUMANIZED);
+    }
+
+    public void save(Component source) {
+        showNotification("Save called from " + source.getId(), NotificationType.HUMANIZED);
     }
 }
