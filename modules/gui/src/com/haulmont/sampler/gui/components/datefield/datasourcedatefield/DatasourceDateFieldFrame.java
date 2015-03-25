0cdff87eec419d27cd86816261a809645b7cb794
Gleb Gorelov <gorelov@haulmont.com>
2015-03-25 11:01:41 +0400

    DateField samples reformat

--- modules/gui/src/com/haulmont/sampler/gui/components/datefield/datasourcedatefield/DatasourceDateFieldFrame.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/datefield/datasourcedatefield/DatasourceDateFieldFrame.java
@@ -1,4 +1,4 @@
-package com.haulmont.sampler.gui.components.datefield;
+package com.haulmont.sampler.gui.components.datefield.datasourcedatefield;
 
 import com.haulmont.cuba.gui.components.AbstractFrame;
 import com.haulmont.cuba.gui.data.Datasource;
