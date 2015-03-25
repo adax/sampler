7dd54bc17d13c4fc6bf7ac0fc52712da6cdefa67
Gleb Gorelov <gorelov@haulmont.com>
2015-03-25 11:26:20 +0400

    FieldGroup samples reformat

--- modules/gui/src/com/haulmont/sampler/gui/components/fieldgroup/simple/SimpleFieldGroupFrame.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/fieldgroup/simple/SimpleFieldGroupFrame.java
@@ -1,4 +1,4 @@
-package com.haulmont.sampler.gui.components.fieldgroup;
+package com.haulmont.sampler.gui.components.fieldgroup.simple;
 
 import com.haulmont.cuba.gui.components.AbstractFrame;
 import com.haulmont.cuba.gui.data.Datasource;
