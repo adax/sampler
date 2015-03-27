3ee32c538fb70cce2da27ad1b757b6311bd1d068
Gleb Gorelov <gorelov@haulmont.com>
2015-03-27 16:49:24 +0400

    TwinColumn samples

---
+++ modules/gui/src/com/haulmont/sampler/gui/components/twincolumn/customoptions/CustomOptionsTwinColumnFrame.java
@@ -0,0 +1,34 @@
+package com.haulmont.sampler.gui.components.twincolumn.customoptions;
+
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.TwinColumn;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomOptionsTwinColumnFrame extends AbstractFrame {
+    @Inject
+    private TwinColumn twinColumnWithOptionsList;
+    @Inject
+    private TwinColumn twinColumnWithOptionsMap;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(4);
+        list.add(5);
+        list.add(7);
+        twinColumnWithOptionsList.setOptionsList(list);
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("two", 2);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("seven", 7);
+        twinColumnWithOptionsMap.setOptionsMap(map);
+    }
+}
