e4b8401244d22ea28d2fe91c21642d99c3291671
Gleb Gorelov <gorelov@haulmont.com>
2015-03-30 18:50:04 +0400

    sample data insertion script

--- modules/core/db/init/hsql/30.create-db.sql
+++ modules/core/db/init/hsql/30.create-db.sql
@@ -1,1 +1,189 @@
+------------------------------------------------------------------------------------------------------------
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Dora', 'Abrams', 47, true, 20, 1, '2015-03-30 09:56:03', null, null, null, '4d750fdb-8ed7-a4e4-88d2-40974615cab2', '2015-03-30 09:56:03', 'admin');
 
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Peter', 'Blakesly', 28, true, 30, 1, '2015-03-30 09:54:07', null, null, null, 'cf8022a3-5b70-520a-d426-aa4614210f0e', '2015-03-30 09:54:07', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Martin', 'Briggs', 23, false, 30, 1, '2015-03-30 09:55:17', null, null, null, 'e7e127d7-33b4-2bd9-d405-0f7923a8bec6', '2015-03-30 09:55:17', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Philipp', 'Brown', 30, true, 30, 1, '2015-03-30 09:52:31', null, null, null, '48eda61c-4142-0e47-5ca8-f338c36cfb0e', '2015-03-30 09:52:31', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('John', 'Doe', 39, false, 30, 1, '2015-03-23 05:21:26', null, null, null, '0c3be785-1125-b755-7e79-3cc136cc1b13', '2015-03-18 06:53:08', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Daniel', 'Evans', 18, true, 10, 1, '2015-03-30 09:58:02', null, null, null, '71ffb579-1b01-614a-0aea-5897c8c3bb91', '2015-03-30 09:58:02', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('John', 'Frost', 50, false, 20, 1, '2015-03-30 09:53:26', null, null, null, '6f218a4d-5ea9-dbf1-b958-c947130a714d', '2015-03-30 09:53:26', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Edward', 'Harris', 27, true, 30, 1, '2015-03-30 10:00:33', null, null, null, '38144502-b2fe-45ab-c183-116aead24aca', '2015-03-30 10:00:33', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('George', 'Hughes', 90, true, 30, 1, '2015-03-30 09:58:56', null, null, null, '8f806608-208b-9deb-138b-9c1dce60d663', '2015-03-30 09:58:56', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Andy', 'Lewis', 35, false, 30, 1, '2015-03-30 09:51:48', null, null, null, '2d59c523-2dc8-7f80-8726-b7fa8caf1c29', '2015-03-30 09:51:48', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('George', 'Moore', 41, true, 10, 1, '2015-03-30 09:51:30', null, null, null, '3fbd8a37-fb90-d5f0-e03e-ae53e41b9fed', '2015-03-30 09:51:30', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Claire', 'Morgan', 25, true, 10, 1, '2015-03-30 09:53:05', null, null, null, '9d9f028c-eade-eee2-d4d8-e97ebb969c54', '2015-03-30 09:53:05', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Katherine', 'Potter', 34, true, 20, 1, '2015-03-30 09:59:26', null, null, null, '068ff1ae-992e-51db-b20f-8e21bcd19bfc', '2015-03-30 09:59:26', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('John', 'Smith', 32, true, 30, 1, '2015-03-30 09:57:33', null, null, null, 'ee875696-0a4d-0d54-41c8-b3dea6e3e7d1', '2015-03-30 09:57:33', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Michael', 'Taylor', 75, false, 10, 1, '2015-03-30 09:58:26', null, null, null, '94d50ae5-1830-174b-81d3-069f221eed3a', '2015-03-30 09:58:26', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Joan', 'Thompson', 20, true, 30, 1, '2015-03-30 10:00:52', null, null, null, 'b6b31099-d194-fb3e-dd51-89d085dcc11c', '2015-03-30 10:00:52', 'admin');
+
+insert into SAMPLER_CUSTOMER
+(NAME, LAST_NAME, AGE, ACTIVE, GRADE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('James', 'White', 56, true, 20, 1, '2015-03-30 09:54:56', null, null, null, '6e3008dd-1eb8-1faf-788d-b6df97130e8d', '2015-03-30 09:54:56', 'admin');
+
+------------------------------------------------------------------------------------------------------------
+
+insert into SAMPLER_PRODUCT
+(NAME, PRICE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('TV set', 24999.00, 1, '2015-03-30 10:08:06', null, null, null, '17bc9b95-b562-0dd2-f904-db6df80b9c71', '2015-03-30 10:08:06', 'admin');
+
+insert into SAMPLER_PRODUCT
+(NAME, PRICE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Keyboard', 499.00, 1, '2015-03-30 10:07:29', null, null, null, '2ab4dece-2f67-69cd-0a76-fe1cd33537f6', '2015-03-30 10:07:29', 'admin');
+
+insert into SAMPLER_PRODUCT
+(NAME, PRICE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Earphones', 2000.00, 1, '2015-03-30 10:09:22', null, null, null, '3d829eba-cdb0-0446-49d4-9c13b1fe8269', '2015-03-30 10:09:22', 'admin');
+
+insert into SAMPLER_PRODUCT
+(NAME, PRICE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Telephone', 17999.00, 1, '2015-03-30 10:08:49', null, null, null, 'd10674cf-a918-b74f-5eeb-c100631320fd', '2015-03-30 10:08:49', 'admin');
+
+insert into SAMPLER_PRODUCT
+(NAME, PRICE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Laptop', 52000.00, 1, '2015-03-30 10:09:36', null, null, null, 'd7496b01-215d-f90c-7442-4d7e8c47848f', '2015-03-30 10:09:36', 'admin');
+
+insert into SAMPLER_PRODUCT
+(NAME, PRICE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Blue-Ray', 7000.00, 1, '2015-03-30 10:07:39', null, null, null, 'd92e7b2b-f1ea-511a-dda0-abf4e8fd7726', '2015-03-30 03:51:28', 'admin');
+
+insert into SAMPLER_PRODUCT
+(NAME, PRICE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Microphone', 4999.00, 1, '2015-03-30 10:09:05', null, null, null, 'd956ed3c-f7ab-687b-382d-0733db863b5c', '2015-03-30 10:09:05', 'admin');
+
+insert into SAMPLER_PRODUCT
+(NAME, PRICE, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('DVD', 1000.00, 1, '2015-03-30 10:07:43', null, null, null, 'dab66821-e3b0-b6a8-eae1-e0b3f0e71d4f', '2015-03-30 03:51:17', 'admin');
+
+------------------------------------------------------------------------------------------------------------
+insert into SAMPLER_TASK
+(NAME, DUE_DATE, ASSIGNEE_ID, PARENT_TASK_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Task1', '2015-03-30 00:00:00', '48eda61c-4142-0e47-5ca8-f338c36cfb0e', null, 1, '2015-03-30 10:23:07', null, null, null, '10685e09-25c8-b3fe-22ba-238304d292b3', '2015-03-20 10:35:27', 'admin');
+
+insert into SAMPLER_TASK
+(NAME, DUE_DATE, ASSIGNEE_ID, PARENT_TASK_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Task2', '2015-03-02 00:00:00', '068ff1ae-992e-51db-b20f-8e21bcd19bfc', null, 1, '2015-03-30 10:23:15', null, null, null, '9b62fc2f-bf83-2a24-d392-0a332e472a01', '2015-03-20 10:35:32', 'admin');
+
+insert into SAMPLER_TASK
+(NAME, DUE_DATE, ASSIGNEE_ID, PARENT_TASK_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Task3', '2015-03-16 00:00:00', '9d9f028c-eade-eee2-d4d8-e97ebb969c54', '10685e09-25c8-b3fe-22ba-238304d292b3', 1, '2015-03-30 10:23:11', null, null, null, '489706de-c25f-c24d-8222-2d9b79189bc8', '2015-03-20 10:35:41', 'admin');
+
+insert into SAMPLER_TASK
+(NAME, DUE_DATE, ASSIGNEE_ID, PARENT_TASK_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Task4', '2015-03-31 00:00:00', 'b6b31099-d194-fb3e-dd51-89d085dcc11c', '489706de-c25f-c24d-8222-2d9b79189bc8', 1, '2015-03-30 10:23:58', null, null, null, 'aa591e3a-7d2b-874d-51c0-3620030c2d64', '2015-03-30 10:23:58', 'admin');
+
+insert into SAMPLER_TASK
+(NAME, DUE_DATE, ASSIGNEE_ID, PARENT_TASK_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Task5', '2015-04-02 00:00:00', 'ee875696-0a4d-0d54-41c8-b3dea6e3e7d1', '9b62fc2f-bf83-2a24-d392-0a332e472a01', 1, '2015-03-30 10:25:02', null, null, null, '4572c65a-3f1e-30a6-b038-bc86ff26cfc9', '2015-03-30 10:24:32', 'admin');
+
+insert into SAMPLER_TASK
+(NAME, DUE_DATE, ASSIGNEE_ID, PARENT_TASK_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('Task6', '2015-04-01 00:00:00', 'e7e127d7-33b4-2bd9-d405-0f7923a8bec6', null, 1, '2015-03-30 10:24:44', null, null, null, '2373294a-f53c-ea74-2c24-c80725b7ba85', '2015-03-30 10:24:44', 'admin');
+
+------------------------------------------------------------------------------------------------------------
+insert into SAMPLER_ORDER
+(CUSTOMER_ID, DATE_, AMOUNT, DESCRIPTION, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('068ff1ae-992e-51db-b20f-8e21bcd19bfc', '2015-03-01', 24990.00, 'Office equipment', 1, '2015-03-30 10:53:36', null, null, null, '23872068-ec86-ba3e-8d9b-a06d4c573aef', '2015-03-18 08:33:32', 'admin');
+
+insert into SAMPLER_ORDER
+(CUSTOMER_ID, DATE_, AMOUNT, DESCRIPTION, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('0c3be785-1125-b755-7e79-3cc136cc1b13', '2015-02-11', 31999.00, 'Home theater', 1, '2015-03-30 10:53:54', null, null, null, '3fbc4ce8-7f8c-dcd0-5788-967f94f6ddbf', '2015-03-18 08:33:22', 'admin');
+
+insert into SAMPLER_ORDER
+(CUSTOMER_ID, DATE_, AMOUNT, DESCRIPTION, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('71ffb579-1b01-614a-0aea-5897c8c3bb91', '2015-03-30', 30998.00, 'Karaoke', 1, '2015-03-30 10:54:58', null, null, null, 'a2ec9471-7e22-56ea-4601-e1d3ae9f24c9', '2015-03-30 10:46:45', 'admin');
+
+insert into SAMPLER_ORDER
+(CUSTOMER_ID, DATE_, AMOUNT, DESCRIPTION, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('ee875696-0a4d-0d54-41c8-b3dea6e3e7d1', '2015-03-26', 59999.00, 'Gamer equipment', 1, '2015-03-30 10:55:19', null, null, null, 'bbb4351b-d605-40c6-55da-d785c7f32e03', '2015-03-18 08:33:43', 'admin');
+
+------------------------------------------------------------------------------------------------------------
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('d7496b01-215d-f90c-7442-4d7e8c47848f', 1.000, 'bbb4351b-d605-40c6-55da-d785c7f32e03', 1, '2015-03-30 10:44:37', null, null, null, '08f26f09-4e6c-125a-12a0-0a8877a57ab2', '2015-03-30 10:44:37', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('d956ed3c-f7ab-687b-382d-0733db863b5c', 1.000, 'bbb4351b-d605-40c6-55da-d785c7f32e03', 1, '2015-03-30 10:44:37', null, null, null, '1e52c338-7e02-058c-fc08-1e00ad56ebf0', '2015-03-30 10:44:37', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('2ab4dece-2f67-69cd-0a76-fe1cd33537f6', 10.000, '23872068-ec86-ba3e-8d9b-a06d4c573aef', 1, '2015-03-30 10:42:52', null, null, null, '3031898a-c8b9-f95a-8977-e97be9a4a8a8', '2015-03-30 10:42:52', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('d92e7b2b-f1ea-511a-dda0-abf4e8fd7726', 1.000, '3fbc4ce8-7f8c-dcd0-5788-967f94f6ddbf', 1, '2015-03-30 10:43:51', null, null, null, '3b5adca3-e01b-2bb0-36be-072165779a30', '2015-03-30 10:43:51', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('3d829eba-cdb0-0446-49d4-9c13b1fe8269', 1.000, 'bbb4351b-d605-40c6-55da-d785c7f32e03', 1, '2015-03-30 10:44:37', null, null, null, '4695cbce-87f4-d034-efde-03c41489d601', '2015-03-30 10:44:37', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('17bc9b95-b562-0dd2-f904-db6df80b9c71', 1.000, 'a2ec9471-7e22-56ea-4601-e1d3ae9f24c9', 1, '2015-03-30 10:46:45', null, null, null, '5eb13363-3fc8-6f79-60a7-be0aa0fe998b', '2015-03-30 10:46:45', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('d956ed3c-f7ab-687b-382d-0733db863b5c', 1.000, 'a2ec9471-7e22-56ea-4601-e1d3ae9f24c9', 1, '2015-03-30 10:54:58', null, null, null, '6d654cd6-dc7a-755d-17a0-bec92b0706b3', '2015-03-30 10:54:58', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('3d829eba-cdb0-0446-49d4-9c13b1fe8269', 10.000, '23872068-ec86-ba3e-8d9b-a06d4c573aef', 1, '2015-03-30 10:43:27', null, null, null, '727cbc21-8b5d-3d49-4daf-86d664f0a737', '2015-03-30 10:43:27', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('dab66821-e3b0-b6a8-eae1-e0b3f0e71d4f', 1.000, 'bbb4351b-d605-40c6-55da-d785c7f32e03', 1, '2015-03-30 10:44:37', null, null, null, 'd27771f8-5bd8-d510-3026-f655c4d7c43a', '2015-03-30 10:44:37', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('dab66821-e3b0-b6a8-eae1-e0b3f0e71d4f', 1.000, 'a2ec9471-7e22-56ea-4601-e1d3ae9f24c9', 1, '2015-03-30 10:54:58', null, null, null, 'df912333-fa7b-2606-c598-1c0135f1247b', '2015-03-30 10:54:58', 'admin');
+
+insert into SAMPLER_INVOICE_ITEM
+(PRODUCT_ID, QUANTITY, ORDER_ID, VERSION, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, ID, CREATE_TS, CREATED_BY)
+values ('17bc9b95-b562-0dd2-f904-db6df80b9c71', 1.000, '3fbc4ce8-7f8c-dcd0-5788-967f94f6ddbf', 1, '2015-03-30 10:37:22', null, null, null, 'e2e78593-5939-2941-4518-f79e47c7f257', '2015-03-30 10:36:55', 'admin');
