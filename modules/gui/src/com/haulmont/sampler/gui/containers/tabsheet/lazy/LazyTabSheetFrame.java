12cd73f4ce17cbbc73c540b2e511409c15af1eb3
Gleb Gorelov <gorelov@haulmont.com>
2015-03-31 17:08:55 +0400

    TabSheet samples

---
+++ modules/gui/src/com/haulmont/sampler/gui/containers/tabsheet/lazy/LazyTabSheetFrame.java
@@ -0,0 +1,46 @@
+package com.haulmont.sampler.gui.containers.tabsheet.lazy;
+
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.Label;
+import com.haulmont.cuba.gui.components.TabSheet;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+public class LazyTabSheetFrame extends AbstractFrame {
+    @Inject
+    private TabSheet tabSheet;
+    @Inject
+    private Label checkMessage;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        tabSheet.addListener(new TabSheet.TabChangeListener() {
+            @Override
+            public void tabChanged(TabSheet.Tab newTab) {
+                checkComponents();
+            }
+        });
+        checkComponents();
+    }
+
+    private void checkComponents() {
+        StringBuilder sb = new StringBuilder();
+
+        Label label1 = getComponent("label1");
+        sb.append("label1 = ");
+        if (label1 != null)
+            sb.append(label1.getValue());
+        else
+            sb.append("null");
+
+        Label label2 = getComponent("label2");
+        sb.append("\nlabel2 = ");
+        if (label2 != null)
+            sb.append(label2.getValue());
+        else
+            sb.append("null");
+
+        checkMessage.setValue(sb.toString());
+    }
+}
