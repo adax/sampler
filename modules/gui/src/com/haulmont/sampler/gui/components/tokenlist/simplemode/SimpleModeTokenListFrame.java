71c5905af8e1d93c6b8c958f64e6a9116f632cb6
Gleb Gorelov <gorelov@haulmont.com>
2015-04-01 15:05:08 +0400

    Samples rename

--- modules/gui/src/com/haulmont/sampler/gui/components/tokenlist/simplemode/SimpleModeTokenListFrame.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/tokenlist/simplemode/SimpleModeTokenListFrame.java
@@ -1,4 +1,4 @@
-package com.haulmont.sampler.gui.components.tokenlist.common;
+package com.haulmont.sampler.gui.components.tokenlist.simplemode;
 
 import com.haulmont.cuba.gui.components.AbstractFrame;
 import com.haulmont.cuba.gui.data.Datasource;
@@ -7,7 +7,7 @@ import com.haulmont.sampler.entity.Order;
 import javax.inject.Inject;
 import java.util.Map;
 
-public class CommonTokenListFrame extends AbstractFrame {
+public class SimpleModeTokenListFrame extends AbstractFrame {
 
     @Inject
     private Datasource<Order> orderDs;
