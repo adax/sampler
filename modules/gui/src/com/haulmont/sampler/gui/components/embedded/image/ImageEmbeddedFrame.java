30fe84961cfc0cb257375cf2ed79993b78221674
Gleb Gorelov <gorelov@haulmont.com>
2015-04-01 11:27:58 +0400

    Embedded samples

--- modules/gui/src/com/haulmont/sampler/gui/components/embedded/image/ImageEmbeddedFrame.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/embedded/image/ImageEmbeddedFrame.java
@@ -5,18 +5,18 @@ import com.haulmont.cuba.core.entity.FileDescriptor;
 import com.haulmont.cuba.core.global.ClientType;
 import com.haulmont.cuba.core.global.FileStorageException;
 import com.haulmont.cuba.gui.AppConfig;
-import com.haulmont.cuba.gui.components.*;
-import com.haulmont.cuba.gui.data.DataSupplier;
-import com.haulmont.cuba.gui.export.ExportDisplay;
-import com.haulmont.cuba.gui.export.ExportFormat;
-import com.haulmont.cuba.gui.upload.FileUploadingAPI;
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.Embedded;
+import com.haulmont.cuba.gui.components.GroupBoxLayout;
 
 import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 
 public class ImageEmbeddedFrame extends AbstractFrame {
 
@@ -25,76 +25,29 @@ public class ImageEmbeddedFrame extends AbstractFrame {
     @Inject
     private Embedded embeddedImage;
     @Inject
-    private FileUploadField uploadField;
-    @Inject
-    private Button clearImageBtn;
-    @Inject
-    private Button downloadImageBtn;
-    @Inject
     private Embedded embeddedImageFile;
     @Inject
-    private DataSupplier dataSupplier;
-    @Inject
     private FileStorageService fileStorageService;
-    @Inject
-    private FileUploadingAPI fileUploading;
-    @Inject
-    private ExportDisplay exportDisplay;
-
-    private FileDescriptor imageFile;
-
-    private float img_height;
-    private float img_width;
 
     @Override
     public void init(Map<String, Object> params) {
-        img_height = groupBoxImage.getHeight() - 60;
-        img_width = groupBoxImage.getWidth() - 60;
-
-        // TODO Может стоит не позволять грузить свои фотки, а подготовить файл в БД заранее?
-        uploadField.addListener(new FileUploadField.ListenerAdapter() {
-            @Override
-            public void uploadSucceeded(Event event) {
-                FileDescriptor fd = uploadField.getFileDescriptor();
-                try {
-                    fileUploading.putFileIntoStorage(uploadField.getFileId(), fd);
-                } catch (FileStorageException e) {
-                    throw new RuntimeException(e);
-                }
-                imageFile = dataSupplier.commit(fd, null);
-                displayImage();
-                updateImageButtons(true);
-            }
-
-            @Override
-            public void uploadFailed(Event event) {
-                showNotification("Upload failed", NotificationType.HUMANIZED);
-            }
-        });
-        updateImageButtons(false);
+        displayImage();
 
         if (AppConfig.getClientType() == ClientType.WEB) {
             embeddedImageFile.setSource("cuba-logo.png");
         }
     }
 
-    public void onDownloadImageBtnClick() {
-        if (imageFile != null)
-            exportDisplay.show(imageFile, ExportFormat.OCTET_STREAM);
-    }
-
-    public void onClearImageBtnClick() {
-        imageFile = null;
-        displayImage();
-        updateImageButtons(false);
-    }
-
-    private void updateImageButtons(boolean enable) {
-        downloadImageBtn.setEnabled(enable);
-        clearImageBtn.setEnabled(enable);
-    }
-
     private void displayImage() {
+        float img_height = groupBoxImage.getHeight() - 60;
+        float img_width = groupBoxImage.getWidth() - 60;
+
+        FileDescriptor imageFile = new FileDescriptor();
+        imageFile.setId(UUID.fromString("61a6a1ee-f13a-f44f-0201-c2f9b3288305"));
+        imageFile.setName("platform-logo.png");
+        imageFile.setCreateDate(new Date(1427857200503L));
+        imageFile.setExtension("png");
+
         byte[] bytes = null;
         if (imageFile != null) {
             try {
