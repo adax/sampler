26d07540aad846d2d621d1823920986d34d52116
Gleb Gorelov <gorelov@haulmont.com>
2015-04-06 09:45:26 +0400

    Startup screen

--- modules/web/src/com/haulmont/sampler/web/SamplerAppWindow.java
+++ modules/web/src/com/haulmont/sampler/web/SamplerAppWindow.java
@@ -3,6 +3,11 @@ package com.haulmont.sampler.web;
 import com.haulmont.cuba.web.AppUI;
 import com.haulmont.cuba.web.AppWindow;
 import com.haulmont.cuba.web.app.folders.FoldersPane;
+import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 
 import javax.annotation.Nullable;
 
@@ -19,5 +24,30 @@ public class SamplerAppWindow extends AppWindow {
     @Override
     protected FoldersPane createFoldersPane() {
         return new LeftPanel(menuBar, this);
+    }
+
+    @Override
+    protected void initStartupScreen() {
+        super.initStartupScreen();
+
+        mainLayout.setMargin(true);
+        mainLayout.setStyleName("sampler-startup-screen");
+
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSpacing(true);
+
+        Image logo = new Image(null, WebComponentsHelper.getResource("images/platform-logo.png"));
+        layout.addComponent(logo);
+        layout.setComponentAlignment(logo, Alignment.TOP_CENTER);
+
+        Label welcomeLabel = new Label(messages.getMessage(getClass(), "StartupScreen.welcome"));
+        welcomeLabel.setStyleName("sampler-startup-screen-welcome");
+        layout.addComponent(welcomeLabel);
+
+//        Label descriptionLabel = new Label(messages.getMessage(getClass(), "StartupScreen.description"));
+//        descriptionLabel.setStyleName("sampler-startup-screen-description");
+//        layout.addComponent(descriptionLabel);
+
+        mainLayout.addComponent(layout);
     }
 }
