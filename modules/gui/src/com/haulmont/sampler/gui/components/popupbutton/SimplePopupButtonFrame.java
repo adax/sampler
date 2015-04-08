6b3fa750ea8d6fb4b356bea69cb8143acb765431
Konstantin Krivopustov <krivopustov@haulmont.com>
2015-04-08 19:02:04 +0400

    Changed PopupButton

--- modules/gui/src/com/haulmont/sampler/gui/components/popupbutton/SimplePopupButtonFrame.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/popupbutton/SimplePopupButtonFrame.java
@@ -1,15 +1,37 @@
 package com.haulmont.sampler.gui.components.popupbutton;
 
-import com.haulmont.cuba.gui.components.AbstractFrame;
-import com.haulmont.cuba.gui.components.Component;
+import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.actions.BaseAction;
+
+import javax.inject.Inject;
+import java.util.Map;
 
 public class SimplePopupButtonFrame extends AbstractFrame {
 
-    public void save1() {
-        showNotification("Save called!", NotificationType.HUMANIZED);
+    @Inject
+    private PopupButton popupButton3;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        popupButton3.addAction(new BaseAction("saveAsDocAction") {
+            @Override
+            public void actionPerform(Component component) {
+                saveAsDoc();
+            }
+        });
+        popupButton3.addAction(new BaseAction("saveAsPdfAction") {
+            @Override
+            public void actionPerform(Component component) {
+                saveAsPdf();
+            }
+        });
     }
 
-    public void save2(Component source) {
-        showNotification("Save called from " + source.getId(), NotificationType.HUMANIZED);
+    public void saveAsDoc() {
+        showNotification("Saved as DOC", NotificationType.HUMANIZED);
+    }
+
+    public void saveAsPdf() {
+        showNotification("Saved as PDF", NotificationType.HUMANIZED);
     }
 }
