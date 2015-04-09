fcda4f3bacda8d0db39b930c0e90e5c6523952ee
Konstantin Krivopustov <krivopustov@haulmont.com>
2015-04-09 12:08:53 +0400

    Changed SearchPickerField

--- modules/gui/src/com/haulmont/sampler/gui/components/searchpickerfield/minlength/MinLengthSearchPickerFieldFrame.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/searchpickerfield/minlength/MinLengthSearchPickerFieldFrame.java
@@ -1,8 +1,7 @@
 package com.haulmont.sampler.gui.components.searchpickerfield.minlength;
 
-import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.AbstractFrame;
 import com.haulmont.cuba.gui.data.Datasource;
-import com.haulmont.cuba.gui.data.ValueListener;
 import com.haulmont.sampler.entity.Order;
 
 import javax.inject.Inject;
@@ -11,23 +10,13 @@ import java.util.Map;
 public class MinLengthSearchPickerFieldFrame extends AbstractFrame {
 
     @Inject
-    private TextField minLength;
-    @Inject
-    private SearchPickerField searchPickerField;
-    @Inject
     private Datasource<Order> orderDs;
 
     @Override
     public void init(Map<String, Object> params) {
+        // Datasource initialization. It is usually done automatically if the screen is
+        // inherited from AbstractEditor and is used as an entity editor.
         Order order = new Order();
         orderDs.setItem(order);
-
-        minLength.setValue(searchPickerField.getMinSearchStringLength());
-        minLength.addListener(new ValueListener() {
-            @Override
-            public void valueChanged(Object source, String property, Object prevValue, Object value) {
-                searchPickerField.setMinSearchStringLength((int) minLength.getValue());
-            }
-        });
     }
 }
--- modules/gui/src/com/haulmont/sampler/gui/components/searchpickerfield/notifications/NotificationsSearchPickerFieldFrame.java
+++ modules/gui/src/com/haulmont/sampler/gui/components/searchpickerfield/notifications/NotificationsSearchPickerFieldFrame.java
@@ -1,8 +1,9 @@
 package com.haulmont.sampler.gui.components.searchpickerfield.notifications;
 
-import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.SearchField;
+import com.haulmont.cuba.gui.components.SearchPickerField;
 import com.haulmont.cuba.gui.data.Datasource;
-import com.haulmont.cuba.gui.data.ValueListener;
 import com.haulmont.sampler.entity.Order;
 
 import javax.inject.Inject;
@@ -11,24 +12,16 @@ import java.util.Map;
 public class NotificationsSearchPickerFieldFrame extends AbstractFrame {
 
     @Inject
-    private TextField minLength;
-    @Inject
     private SearchPickerField searchPickerField;
     @Inject
     private Datasource<Order> orderDs;
 
     @Override
     public void init(Map<String, Object> params) {
+        // Datasource initialization. It is usually done automatically if the screen is
+        // inherited from AbstractEditor and is used as an entity editor.
         Order order = new Order();
         orderDs.setItem(order);
-
-        minLength.setValue(searchPickerField.getMinSearchStringLength());
-        minLength.addListener(new ValueListener() {
-            @Override
-            public void valueChanged(Object source, String property, Object prevValue, Object value) {
-                searchPickerField.setMinSearchStringLength((int) minLength.getValue());
-            }
-        });
 
         searchPickerField.setSearchNotifications(new SearchField.SearchNotifications() {
             @Override
