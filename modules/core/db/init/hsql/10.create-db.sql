32666f6eec0c3d69581eea24a76d3bf033c0a11f
Gleb Gorelov <gorelov@haulmont.com>
2015-10-16 18:50:51 +0400

    Added Gantt Chart | Added localization for entities fields

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -220,3 +220,39 @@ create table SAMPLER_TITLE_VALUE (
     primary key (ID)
 )^
 -- end SAMPLER_TITLE_VALUE
+-- begin SAMPLER_SEGMENT
+create table SAMPLER_SEGMENT (
+    ID varchar(36) not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    VERSION integer,
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    START_ integer,
+    DURATION integer,
+    COLOR varchar(255),
+    TASK_ varchar(255),
+    TASK_SPAN_ID varchar(36),
+    --
+    primary key (ID)
+)^
+-- end SAMPLER_SEGMENT
+-- begin SAMPLER_TASK_SPAN
+create table SAMPLER_TASK_SPAN (
+    ID varchar(36) not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    VERSION integer,
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    CATEGORY varchar(255),
+    --
+    primary key (ID)
+)^
+-- end SAMPLER_TASK_SPAN
---
+++ modules/core/db/update/hsql/151016-1-createTaskSpan.sql
@@ -0,0 +1,14 @@
+create table SAMPLER_TASK_SPAN (
+    ID varchar(36) not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    VERSION integer,
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    CATEGORY varchar(255),
+    --
+    primary key (ID)
+);
