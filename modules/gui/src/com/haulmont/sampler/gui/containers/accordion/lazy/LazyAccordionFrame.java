0e888e79dc20d39cb3c817a9ee642958a0744d9f
Gleb Gorelov <gorelov@haulmont.com>
2016-03-28 17:00:38 +0400

    Accordion samples (draft) #42

---
+++ modules/gui/src/com/haulmont/sampler/gui/containers/accordion/lazy/LazyAccordionFrame.java
@@ -0,0 +1,7 @@
+package com.haulmont.sampler.gui.containers.accordion.lazy;
+
+import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.components.AbstractFrame;
+
+public class LazyAccordionFrame extends AbstractFrame {
+}
